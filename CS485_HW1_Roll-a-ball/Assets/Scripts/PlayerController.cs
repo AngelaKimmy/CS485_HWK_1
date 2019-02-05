ef6dab310f057a460cc5d8e16685f55230dd8078
AngelaKimmy <angelakim.la@gmail.com>
2019-02-04 21:46:27 -0800

    Fixed jumping

--- CS485_HW1_Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ CS485_HW1_Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //For jump
+    public float gravity;
+    public float jumpHeight;
+
     public float speed;
     public Text countText;
     public Text winText;
+
+    //For jump
+    public Transform[] feet = new Transform [6];
+    public LayerMask ground;
+    public Transform foot;
+    private bool isGrounded;
+    private AudioSource jumpSound;
+    private AudioSource collectSound;
+
+    private Vector3 fallingVelocity;
 
     private Rigidbody rb;
     private int count;
@@ -19,6 +33,42 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText();
         winText.text = "";
+
+        //For jump
+        gravity = 9.8f;
+        jumpHeight = 3.0f;
+        fallingVelocity = Vector3.zero;
+    }
+
+    //For jump
+    void Update()
+    {
+        //isGrounded = Physics.CheckSphere(foot.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+        /*
+        for(int i =0; i < 6; i++)
+        {
+            if(Physics.CheckSphere(feet[i].position, 0.1f, ground, QueryTriggerInteraction.Ignore))
+            {
+                isGrounded = true;
+                break;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }*/
+
+        // isGrounded = Physics.CheckSphere(transform.position, 0.1f, ground, QueryTriggerInteraction.Ignore);
+        isGrounded = Physics.CheckSphere(rb.transform.position, 0.8f, ground, QueryTriggerInteraction.Ignore);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+            Debug.Log("I jumped! isGrounded = " + isGrounded);
+        }
+       
+        Debug.Log("End of jump, isGrounded = " + isGrounded);
+
     }
 
     void FixedUpdate()
@@ -36,10 +86,17 @@ public class PlayerController : MonoBehaviour
     { 
         if(other.gameObject.CompareTag("Pick Up"))
         {
+            collectSound = other.gameObject.GetComponent<AudioSource>();
             other.gameObject.SetActive(false);
+            collectSound.Play();
             count++;
             SetCountText();
+
         }
+/*        else if (other.gameObject.CompareTag("Enemy") && isGrounded)
+        {
+            other.gameObject.SetActive(false);
+        }*/
     }
 
     void SetCountText()
